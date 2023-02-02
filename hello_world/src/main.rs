31006955d6aa0bb4c7dacf74d81e588bf0b5c28c
venlit <119433423+venlit@users.noreply.github.com>
2023-02-02 14:57:14 -0500

    Update main.rs

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,20 +1,4 @@
-// mod primenumbers;
-// mod peak_finding;
-// mod ownership;
-// mod struuctures;
-// mod enums;
-// mod option_match;
-// mod string;
-// mod hash_map;
 mod company;
 fn main() {
-    // primenumbers::run();
-    // peak_finding::run();
-    // ownership::run();
-    // struuctures:: run();
-    // enums::run();
-    // option_match::run();
-    // string::run();
-    // hash_map::run();
     company::run();
 }
