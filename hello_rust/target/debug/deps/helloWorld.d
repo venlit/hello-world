2ad5287e2298950aa319a1e94c71e26a33f8c649
venlit <119433423+venlit@users.noreply.github.com>
2023-02-09 10:27:33 -0500

    Delete hello_rust directory

--- hello_rust/target/debug/deps/helloWorld.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Tim\dev\rust_projects\hello_world\target\debug\deps\helloWorld.exe: src\main.rs src\company.rs
-
-C:\Users\Tim\dev\rust_projects\hello_world\target\debug\deps\helloWorld.d: src\main.rs src\company.rs
-
-src\main.rs:
-src\company.rs:
